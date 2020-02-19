e32c371ca4696b0d2e6d14bf6de658cce436fb15
Edward Horsey <edwardhorsey@me.com>
2020-02-19 16:02:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   newarray = []
   
-  while array[counter] < (array.length) + 1 do
+  while array[counter] < ((array.length) + 1) do
     newarray.push(array[counter]**2)
     counter += 1
   end
